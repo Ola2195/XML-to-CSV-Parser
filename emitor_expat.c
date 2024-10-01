7de70385b516d17bc13818b52cee9bb5f8ee495e
Ola2195 <tysiaczka195olas@gmail.com>
2024-10-01 13:55:37 +0200

    Code improvement

--- emitor_expat.c
+++ emitor_expat.c
@@ -36,7 +36,6 @@ typedef struct
     char value[STR_SIZE];
 } Data;
 
-Data data;
 char dataBuffers[ELEMENTS][1024];
 int n_buff = 0;
 
@@ -49,17 +48,17 @@ int n_buff = 0;
  * @param tm   A pointer to a struct containing the current time data (year, month, day, hour).
  * @param str  A pointer to a string buffer where the formatted CSV line will be stored.
  */
-void saveData(struct tm *tm, char *str)
+void saveData(struct tm *tm, Data *data, char *str)
 {
     char oneTag[300];
 
-    strcpy(oneTag, data.emitor);
-    for (int i = 0; i < data.n_tags; i++)
+    strcpy(oneTag, data->emitor);
+    for (int i = 0; i < data->n_tags; i++)
     {
         strcat(oneTag, ".");
-        strcat(oneTag, data.tags[i]);
+        strcat(oneTag, data->tags[i]);
     }
-    sprintf(str, "\"%d-%02d-%02d\",\"%d\",\"%s\",\"%s\"\n", tm->tm_year + 1900, tm->tm_mon + 1, tm->tm_mday, tm->tm_hour, oneTag, data.value);
+    sprintf(str, "\"%d-%02d-%02d\",\"%d\",\"%s\",\"%s\"\n", tm->tm_year + 1900, tm->tm_mon + 1, tm->tm_mday, tm->tm_hour, oneTag, data->value);
 }
 
 /**
@@ -68,19 +67,32 @@ void saveData(struct tm *tm, char *str)
  * The function retrieves the current system time, formats the data using saveData(),
  * and increments the buffer counter for storing the next entry.
  */
-void saveOneElement(void)
+void saveOneElement(Data *data)
 {
     time_t t = time(NULL); // System time variable
     struct tm tm = *localtime(&t);
 
-    saveData(&tm, dataBuffers[n_buff]);
+    saveData(&tm, data, dataBuffers[n_buff]);
     if (n_buff < ELEMENTS - 1)
     {
         n_buff++;
     }
     else
     {
-        perror("Przekroczono limit bufforów!");
+        fprintf(stderr, "Przekroczono limit bufforów!");
+    }
+}
+
+void addTag(Data *data, const char *tag)
+{
+    if (data->n_tags < MAX_TAG - 1)
+    {
+        strcpy(data->tags[data->n_tags], tag);
+        data->n_tags++;
+    }
+    else
+    {
+        fprintf(stderr, "Przekroczono limit tagów!\n");
     }
 }
 
@@ -97,66 +109,44 @@ void saveOneElement(void)
  */
 void XMLCALL startElement(void *userData, const char *name, const char **attr)
 {
+    Data *data = (Data *)userData;
+
     if (!strcmp(name, "emitor"))
     {
         for (int i = 0; attr[i]; i += 2)
         {
             if (strcmp(attr[i], "nazwa") == 0)
             {
-                strcpy(data.emitor, attr[i + 1]);
+                strcpy(data->emitor, attr[i + 1]);
             }
         }
     }
     else if (!strcmp(name, "status") || !strcmp(name, "parametr") || !strcmp(name, "stezenie"))
     {
-        data.n_tags = 0;
-        strcpy(data.tags[data.n_tags], name);
-        if (data.n_tags < MAX_TAG - 1)
-        {
-            data.n_tags++;
-        }
-        else
-        {
-            perror("Przekroczono limit tagów!");
-        }
+        data->n_tags = 0;
+        addTag(data, name);
 
         for (int i = 0; attr[i]; i += 2)
         {
             if (strcmp(attr[i], "typ") == 0)
             {
-                strcpy(data.tags[data.n_tags], attr[i + 1]);
-                if (data.n_tags < MAX_TAG - 1)
-                {
-                    data.n_tags++;
-                }
-                else
-                {
-                    perror("Przekroczono limit tagów!");
-                }
+                addTag(data, attr[i + 1]);
                 break;
             }
         }
     }
-    else if (data.n_tags != 0 && (!strcmp(name, "wartosc") || !strcmp(name, "auto") || !strcmp(name, "reka")))
+    else if (data->n_tags != 0 && (!strcmp(name, "wartosc") || !strcmp(name, "auto") || !strcmp(name, "reka")))
     {
-        strcpy(data.tags[data.n_tags], name);
-        if (data.n_tags < MAX_TAG - 1)
-        {
-            data.n_tags++;
-        }
-        else
-        {
-            perror("Przekroczono limit tagów!");
-        }
+        addTag(data, name);
         for (int i = 0; attr[i]; i += 2)
         {
             if (strcmp(attr[i], "pkt") == 0)
             {
-                strcpy(data.value, attr[i + 1]);
+                strcpy(data->value, attr[i + 1]);
                 break;
             }
         }
-        saveOneElement();
+        saveOneElement(data);
     }
 }
 
@@ -168,9 +158,10 @@ void XMLCALL startElement(void *userData, const char *name, const char **attr)
  */
 void XMLCALL endElement(void *userData, const char *name)
 {
-    if (data.n_tags > 0)
+    Data *data = (Data *)userData;
+    if (data->n_tags > 0)
     {
-        data.n_tags--;
+        data->n_tags--;
     }
 }
 
@@ -179,7 +170,7 @@ void XMLCALL endElement(void *userData, const char *name)
  *
  * @param userData  A pointer to the user data (passed from the parser).
  * @param s         A pointer to the text data (string) contained in the XML element.
- * @param len       Length of the text data.
+ * @param len       Length of the text data->
  */
 void XMLCALL characterData(void *userData, const XML_Char *s, int len)
 {
@@ -188,20 +179,22 @@ void XMLCALL characterData(void *userData, const XML_Char *s, int len)
 
 int main()
 {
+    Data data;
+
     /*
      * Support for external XML and CSV files.
      */
     FILE *inputFile = fopen(INPUT_FILENAME, "r");
     if (!inputFile)
     {
-        perror("Nie można otworzyć pliku z danymi.\n");
+        fprintf(stderr, "Nie można otworzyć pliku z danymi.\n");
         return EXIT_FAILURE;
     }
 
     FILE *outputFile = fopen(OUTPUT_FILENAME, "w");
     if (outputFile == NULL)
     {
-        perror("Nie można otworzyć pliku wynikowego.\n\n");
+        fprintf(stderr, "Nie można otworzyć pliku wynikowego.\n\n");
         fclose(inputFile);
         return EXIT_FAILURE;
     }
@@ -210,6 +203,7 @@ int main()
     XML_Parser parser = XML_ParserCreate(NULL);
     XML_SetElementHandler(parser, startElement, endElement);
     XML_SetCharacterDataHandler(parser, characterData);
+    XML_SetUserData(parser, &data);
 
     printf("\"YYYY-MM-DD\",\"Hour\",\"Emitor.Tags\",\"Pkt_Value\"\n");
     if (fprintf(outputFile, "\"YYYY-MM-DD\",\"Hour\",\"Emitor.Tags\",\"Pkt_Value\"\n") < 0)
