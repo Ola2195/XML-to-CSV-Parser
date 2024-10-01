7c9c9ea7b64baf4588f43207cc42171eefce72b9
Ola2195 <tysiaczka195olas@gmail.com>
2024-10-01 14:03:16 +0200

    New commants

--- emitor_expat.c
+++ emitor_expat.c
@@ -20,13 +20,13 @@
 
 #define INPUT_FILENAME "example.xml"
 #define OUTPUT_FILENAME "wyniki.csv"
-#define ELEMENTS 30
-#define MAX_TAG 5
+#define ELEMENTS 30     // Maximum number of data elements that can be stored in the buffer
+#define MAX_TAG 5       // Maximum number of tags associated with one emitter
 
-#define STR_SIZE 15
+#define STR_SIZE 15     // Maximum length of strings for emitter names, tags, and values
 
 /*
- * Structure of parsed data
+ * Structure to store parsed data from the XML file.
  */
 typedef struct
 {
@@ -36,16 +36,21 @@ typedef struct
     char value[STR_SIZE];
 } Data;
 
+/*
+ * Array to store formatted data entries in CSV format, before writing to a file.
+ * Each entry is stored as a string.
+ */
 char dataBuffers[ELEMENTS][1024];
-int n_buff = 0;
+int n_buff = 0;     // Counter for the number of elements stored in the buffer
 
 /**
- * @brief   Formats and saves the collected data into a CSV format.
+ * @brief   Formats and saves the collected data into CSV format.
  *
  * The function concatenates the 'emitor' and its associated tags into a single string,
- * then formats the data into a CSV string with a timestamp (YYYY-MM-DD, Hour), tags, and value.
+ * then formats the data into a CSV string with a timestamp (YYYY-MM-DD, Hour), emitter tags, and value.
  *
  * @param tm   A pointer to a struct containing the current time data (year, month, day, hour).
+ * @param data A pointer to the Data struct containing emitter and tag information.
  * @param str  A pointer to a string buffer where the formatted CSV line will be stored.
  */
 void saveData(struct tm *tm, Data *data, char *str)
@@ -58,18 +63,22 @@ void saveData(struct tm *tm, Data *data, char *str)
         strcat(oneTag, ".");
         strcat(oneTag, data->tags[i]);
     }
-    sprintf(str, "\"%d-%02d-%02d\",\"%d\",\"%s\",\"%s\"\n", tm->tm_year + 1900, tm->tm_mon + 1, tm->tm_mday, tm->tm_hour, oneTag, data->value);
+    sprintf(str, "\"%d-%02d-%02d\",\"%d\",\"%s\",\"%s\"\n",
+            tm->tm_year + 1900, tm->tm_mon + 1, tm->tm_mday, tm->tm_hour,
+            oneTag, data->value);
 }
 
 /**
- * @brief   Saves one element of data by appending a timestamp and calling the saveData() function.
+ * @brief   Adds a new element of data, appending a timestamp and calling saveData().
  *
  * The function retrieves the current system time, formats the data using saveData(),
  * and increments the buffer counter for storing the next entry.
+ *
+ * @param data  A pointer to the Data struct containing parsed XML data to be saved.
  */
 void saveOneElement(Data *data)
 {
-    time_t t = time(NULL); // System time variable
+    time_t t = time(NULL);
     struct tm tm = *localtime(&t);
 
     saveData(&tm, data, dataBuffers[n_buff]);
@@ -83,6 +92,14 @@ void saveOneElement(Data *data)
     }
 }
 
+/**
+ * @brief   Adds a tag to the Data struct, checking for the maximum allowed tags.
+ *
+ * The function appends a tag to the tags array in the Data struct if there is space available.
+ *
+ * @param data  A pointer to the Data struct where the tag will be added.
+ * @param tag   A string representing the tag to be added.
+ */
 void addTag(Data *data, const char *tag)
 {
     if (data->n_tags < MAX_TAG - 1)
@@ -99,11 +116,11 @@ void addTag(Data *data, const char *tag)
 /**
  * @brief   Function called when the parser encounters the beginning of an XML element.
  *
- * The function handles the beginning of an XML element, such as 'status', 'value' or 'status'.
- * Assigns attribute values to the data structure to be saved later.
- * Increases the counter of collected data when the corresponding element is found.
+ * This function handles the start of an XML element and extracts relevant data such as
+ * emitter names, tags, and values from the element's attributes.
+ * It assigns attribute values to the Data struct and prepares data for saving when needed.
  *
- * @param userData  A pointer to the user data (can be used to pass additional information to the parser, such as the resulting file).
+ * @param userData  A pointer to user data (the Data struct in this case).
  * @param name      Name of the currently processed XML element.
  * @param attr      An array of attributes of the XML element (alternating name and attribute value).
  */
@@ -153,7 +170,9 @@ void XMLCALL startElement(void *userData, const char *name, const char **attr)
 /**
  * @brief   Function called when the parser encounters the end of an XML element.
  *
- * @param userData  A pointer to the user data (passed from the parser).
+ * This function decreases the tag counter when an XML element ends.
+ *
+ * @param userData  A pointer to user data (the Data struct in this case).
  * @param name      Name of the currently terminated XML element.
  */
 void XMLCALL endElement(void *userData, const char *name)
@@ -168,9 +187,12 @@ void XMLCALL endElement(void *userData, const char *name)
 /**
  * @brief   Function called when the parser encounters text data in an XML element.
  *
- * @param userData  A pointer to the user data (passed from the parser).
+ * This function is not currently implemented, but it could be used to handle text content
+ * within XML elements.
+ *
+ * @param userData  A pointer to user data (the Data struct in this case).
  * @param s         A pointer to the text data (string) contained in the XML element.
- * @param len       Length of the text data->
+ * @param len       Length of the text data.
  */
 void XMLCALL characterData(void *userData, const XML_Char *s, int len)
 {
@@ -179,7 +201,7 @@ void XMLCALL characterData(void *userData, const XML_Char *s, int len)
 
 int main()
 {
-    Data data;
+    Data data;  // Initialize the data structure to store parsed information
 
     /*
      * Support for external XML and CSV files.
@@ -199,12 +221,13 @@ int main()
         return EXIT_FAILURE;
     }
 
-    // Parser initialization
+    // Initialize the XML parser
     XML_Parser parser = XML_ParserCreate(NULL);
     XML_SetElementHandler(parser, startElement, endElement);
     XML_SetCharacterDataHandler(parser, characterData);
     XML_SetUserData(parser, &data);
 
+    // Write the CSV header to both the console and the output file
     printf("\"YYYY-MM-DD\",\"Hour\",\"Emitor.Tags\",\"Pkt_Value\"\n");
     if (fprintf(outputFile, "\"YYYY-MM-DD\",\"Hour\",\"Emitor.Tags\",\"Pkt_Value\"\n") < 0)
     {
@@ -215,7 +238,7 @@ int main()
         return EXIT_FAILURE;
     }
 
-    // XML file parsing
+    // Read the XML file and parse its contents
     char buffer[1024];
     int bytesRead;
     while ((bytesRead = fread(buffer, 1, sizeof(buffer), inputFile)) > 0)
